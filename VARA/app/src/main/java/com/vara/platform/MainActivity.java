8a8203aee109d68d0f4121a3d549bd99d6be0926
burningman2k20 <burningman2k9@gmail.com>
2020-08-10 14:00:53 -0400

    my updates

--- VARA/app/src/main/java/com/vara/platform/MainActivity.java
+++ VARA/app/src/main/java/com/vara/platform/MainActivity.java
@@ -2,6 +2,7 @@ package com.vara.platform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -10,22 +11,28 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button homeButton;
+    Button okbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        setContentView(R.layout.activity_main);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
-        setContentView(R.layout.logopage);
-        Button button = (Button) findViewById(R.id.okButton);
-        Button homeButton = (Button) findViewById(R.id.homeButton);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        okbutton = (Button) findViewById(R.id.okButton);
+        //homeButton = (Button) findViewById(R.id.homeButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+        okbutton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.loginpage);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                //setContentView(R.layout.loginpage);
                 //Toast.makeText(MainActivity.this, "You clicked on ImageView", Toast.LENGTH_LONG).show();
 
             }
---
+++ VARA/app/src/main/java/com/vara/platform/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.vara.platform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button homeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getSupportActionBar().hide(); // hide the title bar
+        homeButton = (Button) findViewById(R.id.homeButton);
+
+
+         homeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                setContentView(R.layout.logopage);
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                //Toast.makeText(MainActivity.this, "You clicked on ImageView", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+//    public void goHome(){
+//        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+//        startActivity(intent);
+//
+//    }
+}
