15e70a6580d3ff91097f05dad381cd9299d7693b
burningman2k20 <burningman2k9@gmail.com>
2020-08-10 15:12:36 -0400

    updae

--- VARA/app/src/main/java/com/vara/platform/LoginActivity.java
+++ VARA/app/src/main/java/com/vara/platform/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     Button homeButton;
@@ -30,9 +31,10 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-//    public void goHome(){
+    public void doSomething(){
 //        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
 //        startActivity(intent);
+        Toast.makeText(LoginActivity.this, "Did something", Toast.LENGTH_LONG).show();
 //
-//    }
+    }
 }
