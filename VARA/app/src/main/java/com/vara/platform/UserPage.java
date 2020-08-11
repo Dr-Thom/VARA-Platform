1d451e1fef278b367aac41f2e0f1eca2c882e4c1
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-11 16:59:05 -0400

    added xml file

---
+++ VARA/app/src/main/java/com/vara/platform/UserPage.java
@@ -0,0 +1,14 @@
+package com.vara.platform;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_page);
+    }
+}
