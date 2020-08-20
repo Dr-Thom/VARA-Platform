ffe264e1666ce3f3d06a916379b083fe94e9ac4e
RoaaAlmadi <roaa.almadi@gmail.com>
2020-08-20 13:34:39 -0700

    fixed the FarewellActivaty.java

--- VARA/app/src/main/java/com/vara/platform/FarewellActivity.java
+++ VARA/app/src/main/java/com/vara/platform/FarewellActivity.java
@@ -1,6 +1,31 @@
 package com.vara.platform;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FarewellActivity extends AppCompatActivity {
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //if (DBHelper.getUser() != null) {
+        setContentView(R.layout.activity_farewell);
+
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(FarewellActivity.this, AdsDisplay.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 15000);
+        //} else { return; }
+    }
 }
