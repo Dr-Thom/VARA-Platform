c6bfe8e484ad2eb453d6e619fb2c947eee2a8bd6
burningman2k20 <burningman2k9@gmail.com>
2020-08-21 13:00:49 -0400

    reverted to older coe

--- VARA/app/src/main/java/com/vara/platform/AdsDisplay.java
+++ VARA/app/src/main/java/com/vara/platform/AdsDisplay.java
@@ -1,10 +1,8 @@
 package com.vara.platform;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.google.android.ads.nativetemplates.TemplateView;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
@@ -12,49 +10,37 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.formats.UnifiedNativeAd;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class AdsDisplay extends AppCompatActivity {
     Timer timer;
-    private com.google.android.gms.ads.AdView mAdView;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ads_display);
-
-        getSupportActionBar().hide();
-    
-        java.util.List< String> testDeviceIds = java.util.Arrays.asList(AdRequest.DEVICE_ID_EMULATOR);
-        com.google.android.gms.ads.RequestConfiguration configuration =
-                new com.google.android.gms.ads.RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
-        MobileAds.setRequestConfiguration(configuration);
-
-//        mAdView = findViewById(R.id.adView);
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
         
+        getSupportActionBar().hide();
+        //ads code
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
-
+            
             }
         });
         AdLoader.Builder builder = new AdLoader.Builder(this, getString(R.string.nativeas_ad_unit_id));
         builder.forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
             @Override
             public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-                TemplateView templateView = findViewById( com.vara.platform.R.id.ads_view);
+                TemplateView templateView = findViewById(R.id.ads_view);
                 templateView.setNativeAd(unifiedNativeAd);
-
             }
         });
         AdLoader adLoader = builder.build();
         AdRequest adRequest = new AdRequest.Builder().build();
         adLoader.loadAd(adRequest);
-
+        //next screen code
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -64,6 +50,8 @@ public class AdsDisplay extends AppCompatActivity {
                 finish();
             }
         }, 60000);
-
     }
 }
+
+
+
